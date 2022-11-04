d04277f403e2f549c5538175134e5d1ccd93023e
DraconMarius <113488982+DraconMarius@users.noreply.github.com>
2022-11-04 01:55:32 -0700

    added get product by specific ID - route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -61,8 +61,22 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const id = req.params.id;
+    const newName = req.body.category_name;
+    const updCat = await Category.destroy(
+      {
+        where: {
+          id: id
+        }
+      }
+    );
+    res.status(200).json(`ID: ${id} has been deleted`)
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -3,16 +3,32 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
-// get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  try {
+    const prodData = await Product.findAll({
+      // be sure to include its associated Category and Tag data
+      include: [{ model: Category }, { model: Tag }]
+    });
+    res.status(200).json(prodData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  try {
+    // be sure to include its associated Category and Tag data
+    const id = req.params.id;
+    const prodData = await Product.findByPk(id, {
+      include: [{ model: Category }, { model: Tag }]
+    });
+    res.status(200).json(prodData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
